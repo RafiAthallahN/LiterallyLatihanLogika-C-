1792558e88d130806e5ac82b1665dbb481a25f7a
RafiAthallahN <75708409+RafiAthallahN@users.noreply.github.com>
2022-04-12 11:39:01 +0700

    tes

---
+++ test1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  	int bil1;
+    int bil2;
+	cout<<"Masukan Bilangan 1 : ";
+	cin>>bil1;
+  cout<<"Masukan Bilangan 2 : ";
+	cin>>bil2;
+	
+
+	if (bil2 < bil1){
+		cout<<bil1;
+	}else if (bil2 < bil1){
+		cout<<bil1;
+  }
+  
+}
---
+++ test2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int bil;
+	cout<<"Masukan Bilangan : ";
+	cin>>bil;
+	
+
+	if (bil%2==0){
+		cout<<"genap";
+	}else {
+		cout<<"ganjil";	
+	}
+}
